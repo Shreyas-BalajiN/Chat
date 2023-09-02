86f9ba78039e64715e9230b04894d83eac0c588a
Adriana Sanchez Gomez <ad897964@dal.ca>
2023-09-02 15:08:43 -0300

    Added custom websocket handler and configuration

--- backend/src/main/java/com/textitout/chat/sockets/WebSocketsConfiguration.java
+++ backend/src/main/java/com/textitout/chat/sockets/WebSocketsConfiguration.java
@@ -7,8 +7,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
-@Configuration
-@EnableWebSocketMessageBroker
+
 public class WebSocketsConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
